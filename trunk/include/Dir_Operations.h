d276cc596c275f1278baa33dae39d5aaa2131b40
dm.project.14@gmail.com <dm.project.14@gmail.com@11901e7a-72e8-f9d2-8eec-96eb48902244>
2012-11-29 23:34:02 +0000

    suhas

--- trunk/include/Dir_Operations.h
+++ trunk/include/Dir_Operations.h
@@ -5,3 +5,7 @@ void make_dir ( char *parent_path, char *dir_name);
 void delete_dir ( char *path );
 void move_dir ( char *source_dir_path, char *dest_dir_path );
 void list_dir ( char *dir_name, int flag, char *output_file_name );
+
+
+char *listarr[400];
+int listindex;
